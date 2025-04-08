4b893cbdcc8a4aa6b11c0efe4f079c19e10b6365
Ntshebetseng Moloi <nana13.moloi@gmail.com>
2025-04-08 10:40:11 +0200

    Add project files.

---
+++ Guessing_Game/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guessing_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("i am thinking of a number!");
+            Console.WriteLine("can you guess what it is?");
+
+            
+            Console.ReadKey();
+        }
+
+        
+    }
+}
