f11ec1fac1d2ff6a1a6e3648831798abd968f668
Ntshebetseng Moloi <nana13.moloi@gmail.com>
2025-04-08 11:21:16 +0200

    add basic number guessing game with input validation

--- Guessing_Game/Program.cs
+++ Guessing_Game/Program.cs
@@ -10,13 +10,67 @@ namespace Guessing_Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("i am thinking of a number!");
-            Console.WriteLine("can you guess what it is?");
+            Console.WriteLine("I'm thinking of a number between 0 and 100.");
+            Console.WriteLine("Can you guess what it is?");
+            PlayGame();
+           
+           
+        }
+        
+
+        static void PlayGame()
+        {
+            Random random = new Random();
+            int secretNumber = random.Next(0, 101);
+            int guess = 0;
+            int attempts = 0;
+
+            while (guess != secretNumber)
+            {
+                Console.Write("\nWhats your guess?: ");
+                string input = Console.ReadLine();
+
+                
+                if (int.TryParse(input, out guess))
+                {
+                    attempts++;
+
+                    if (guess < secretNumber)
+                    {
+                        Console.WriteLine("Too low! Try again.");
+                    }
+                    else if (guess > secretNumber)
+                    {
+                        Console.WriteLine("Too high! Try again.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nYipee! The number was {secretNumber}.");
+                        Console.WriteLine($"You guessed it in {attempts} attempts.");
+                        
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("❌ Invalid input. Please enter a number.");
+                }
+            }
 
             
-            Console.ReadKey();
+            PlayAgain();
         }
-
-        
+        static void PlayAgain()
+        {
+            Console.Write("\nWould you like to play again? (yes or no): ");
+            string input = Console.ReadLine()?.ToLower();
+            if(input == "yes" ||input == "y")
+            {
+                PlayGame();
+            }
+            else
+            {
+                Console.WriteLine("\nThanks for playing!");
+            }
+        }
     }
 }
